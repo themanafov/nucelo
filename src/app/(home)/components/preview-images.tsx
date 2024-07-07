d41ab9090298a48acd64460146e73edfb8d3113d
Manafov <mehmanmanafov@icloud.com>
2024-07-07 17:58:09 +0400

    Update preview-images.tsx

--- src/app/(home)/components/preview-images.tsx
+++ src/app/(home)/components/preview-images.tsx
@@ -12,7 +12,7 @@ export default function PreviewImages() {
     const [preview, setPreview] = useState<Preview>(marketingConfig.previews[0])
     return (
         <div className="flex flex-col">
-            <div className="border border-gray-2 rounded-t-md h-[600px] overflow-hidden">
+            <div className="border border-gray-2 rounded-t-md h-[600px] max-md:h-[350px] overflow-hidden">
                 <Image
                     src={preview.image}
                     alt={preview.title}
@@ -25,7 +25,7 @@ export default function PreviewImages() {
                 />
             </div>
             <div className="flex flex-col border border-gray-2 p-1 border-t-0 rounded-t-none gap-3 transition-all overflow-hidden rounded-md">
-                <div className="flex gap-1">
+                <div className="grid grid-cols-5 gap-1 max-md:grid-cols-2 max-sm:grid-cols-1">
                     {marketingConfig.previews.map(p => (
                         <Button variant="ghost" size="sm" className={cn("bg-transparent w-full whitespace-nowrap rounded-md gap-2", preview.title === p.title ? "bg-gray-2 text-secondary" : "")} onClick={() => setPreview(p)} key={p.title}>
                             {p.title}
