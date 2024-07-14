e5e225c633f3350cc6fa1fc853a761f263861d48
Manafov <mehmanmanafov@icloud.com>
2024-07-15 02:12:09 +0400

    Update publish-button.tsx

--- src/components/editor/page/publish-button.tsx
+++ src/components/editor/page/publish-button.tsx
@@ -117,7 +117,6 @@ export default function PublishButton({
                       </>
                     )}
                   </Button>
-                  ``
                 </TooltipTrigger>
                 {!post.published && (
                   <TooltipContent>
