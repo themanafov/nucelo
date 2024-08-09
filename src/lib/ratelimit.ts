135f9402d946fb89df21debec0bd967cf5dd6a8d
Manafov <mehmanmanafov@icloud.com>
2024-08-09 22:13:23 +0400

    Update ratelimit.ts

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -1,22 +1,25 @@
 import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis"
 
-const redis = kv;
+const redis = Redis.fromEnv();
 
 export const rateLimit = {
   bookmark: new Ratelimit({
     redis,
     limiter: Ratelimit.slidingWindow(1, "1 h"),
     prefix: "clicks",
+    analytics: true,
   }),
   analytics: new Ratelimit({
     redis,
     limiter: Ratelimit.slidingWindow(1, "5 h"),
     prefix: "analytics",
+    analytics: true,
   }),
   subscribe: new Ratelimit({
     redis,
     limiter: Ratelimit.slidingWindow(5, "5 h"),
     prefix: "subscribe",
+    analytics: true,
   }),
 };
